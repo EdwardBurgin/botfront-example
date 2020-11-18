268cfd38bff82909191289ef666faa0a3b61b5f2
Raj Nath Patel <raj.nath.patel@huawei.com>
2020-11-18 16:53:09 +0000

    first commit

---
+++ actions/actions.py
@@ -0,0 +1,22 @@
+##################################################################################
+#  You can add your actions in this file or create any other file in this folder #
+##################################################################################
+
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet, ReminderScheduled, ConversationPaused, ConversationResumed, FollowupAction, Restarted, ReminderScheduled
+from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
+from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
+
+
+class ActionMyKB(ActionQueryKnowledgeBase):
+    def __init__(self):
+        # load knowledge base with data from the given file
+        knowledge_base = InMemoryKnowledgeBase("knowledge_base_data.json")
+
+        # overwrite the representation function of the hotel object
+        # by default the representation function is just the name of the object
+        knowledge_base.set_representation_function_of_object(
+            "hotel", lambda obj: obj["name"] + " (" + obj["city"] + ")"
+        )
+
+        super().__init__(knowledge_base)
---
+++ rasa/extensions/nlu/extractors/custom_spacy_entity_extractor_remote.py
@@ -0,0 +1,71 @@
+from rasa.nlu.extractors.extractor import EntityExtractor
+from typing import Any, Dict, List, Text, Optional, Type
+from rasa.nlu.constants import ENTITIES, TEXT, INTENT
+
+
+class RemoteSpacyCustomNER(EntityExtractor):
+    """A custom sentiment analysis component"""
+    name = "custom_entities"
+    provides = ["custom_entities"]
+    language_list = ["en"]
+    print('initialised the class')
+
+    defaults = {
+        "host": 'custom-ner',
+        "port": 9501,
+        "arch": None,
+        "dropout": 0.1,
+        "accumulate_gradient": 1,
+        "patience": 500,
+        "max_epochs": 0,
+        "max_steps": 5000,
+        "eval_frequency": 200,
+        # by default all dimensions recognized by spacy are returned
+        # dimensions can be configured to contain an array of strings
+        # with the names of the dimensions to filter for
+        "dimensions": None
+    }
+
+    def __init__(
+            self, component_config: Dict[Text, Any] = None, nlp: "Language" = None
+    ) -> None:
+        self.nlp = nlp
+        self.component_config = component_config
+        super(RemoteSpacyCustomNER, self).__init__(component_config)
+
+    def train(self, training_data, cfg, **kwargs):
+        """Load the sentiment polarity labels from the text
+           file, retrieve training tokens and after formatting
+           data train the classifier."""
+        entities = []
+        texts = []
+        for example in training_data.entity_examples:
+            texts.append(example.get(TEXT))
+            entities.append(example.get(ENTITIES))
+
+        import requests
+        url = 'http://{0}:{1}/train'.format(self.component_config.get('host'),
+                                            self.component_config.get('port'))
+        data = {"text": texts, "entities": entities, "params": [self.component_config]}
+        print(requests.put(url, json=data).text)
+
+    def process(self, message, **kwargs):
+        """Retrieve the tokens of the new message, pass it to the classifier
+            and append prediction results to the message class."""
+        import requests
+        url = 'http://{0}:{1}/predict'.format(self.component_config.get('host'),
+                                              self.component_config.get('port'))
+        data = {"text": [message.get(TEXT)]}
+        response = requests.get(url, json=data)
+        json_response = response.json()
+        all_extracted = self.add_extractor_name(json_response['entities'][0])
+        dimensions = self.component_config["dimensions"]
+        extracted = RemoteSpacyCustomNER.filter_irrelevant_entities(
+            all_extracted, dimensions
+        )
+
+        message.set(ENTITIES, message.get(ENTITIES, []) + extracted, add_to_output=True)
+
+    def persist(self, file_name, model_dir):
+        """Pass because a pre-trained model is already persisted"""
+        pass
